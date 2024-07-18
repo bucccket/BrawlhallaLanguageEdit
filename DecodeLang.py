fb1056aec770a470522a17ae263019c1cdce38bd
Bucccket <37452311+bucccket@users.noreply.github.com>
2024-07-18 14:15:02 +0200

    added reading from file feature

--- DecodeLang.py
+++ DecodeLang.py
@@ -1,63 +1,69 @@
+import re
+from typing import Any
 import zlib
 import io
 import codecs
 
+
 class UTF8String:
-    
-    #ctor
-    def __init__(self, length, string):
+
+    # ctor
+    def __init__(self, length: int, string: str):
         self.length = length
         self.string = string
-        
-    #static class methods    
+
+    # static class methods
     @classmethod
-    def FromBytesIO(cls, data):
+    def FromBytesIO(cls, data: io.BytesIO) -> Any:
         length = cls.__ReadUint16BE(data)
-        string = data.read(length).decode('utf-8')
+        res = data.read(length)
+        string = res.decode('utf-8')
         return cls(length, string)
-    
+
     @classmethod
-    def FromString(cls, string):
-        return cls(len(string), string)    
-    
-    #public
-    def WriteBytesIO(self, data):
+    def FromString(cls, string: str) -> Any:
+        return cls(len(string), string)
+
+    # public
+    def WriteBytesIO(self, data: io.BytesIO) -> None:
         data.write(self.length.to_bytes(2, byteorder="big"))
         data.write(self.string.encode('utf-8'))
-        
-    #private
-    def __ReadUint16BE(data):
+
+    # private
+    def __ReadUint16BE(data: io.BytesIO) -> int:
         byte = data.read(2)
         return int.from_bytes(byte, byteorder="big")  # whar?
 
+
 class Entry:
-    
-    #ctor
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-    
-    #public
-    def WriteBytesIO(self, data):
+
+    # ctor
+    def __init__(self, key: str, value: str):
+        self.key: UTF8String = key
+        self.value: UTF8String = value
+
+    # public
+    def WriteBytesIO(self, data: io.BytesIO) -> None:
         self.key.WriteBytesIO(data)
         self.value.WriteBytesIO(data)
-        
-    def SetValue(self, value):
+
+    def SetValue(self, value: str) -> None:
         self.value = UTF8String.FromString(value)
-        
-    #static class methods
+
+    # static class methods
     @classmethod
-    def FromBytesIO(cls, data):
+    def FromBytesIO(cls, data: io.BytesIO) -> Any:
         return cls(UTF8String.FromBytesIO(data), UTF8String.FromBytesIO(data))
-    
+
     @classmethod
-    def FromKeyValuePair(cls, key, value):
+    def FromKeyValuePair(cls, key: str, value: str) -> Any:
         return cls(UTF8String.FromString(key), UTF8String.FromString(value))
-    
+
+
 class LangFile:
 
-    #ctor
-    def __init__(self, filename):
+    # ctor
+    def __init__(self, filename: str):
         fd = open(filename, "rb")
 
         self.entries = []
@@ -67,51 +73,62 @@ class LangFile:
 
         self.__ParseFile()
 
-    #public 
-    def Save(self, filename):
-        
-        data = io.BytesIO()
+    # public
+    def Save(self, filename: str) -> None:
+
+        data: io.BytesIO = io.BytesIO()
         data.write(self.__WriteUint32BE(self.entry_count))
         for entry in self.entries:
-            entry.WriteBytesIO(data)       
-        
+            entry.WriteBytesIO(data)
+
         with open(filename, "wb") as fd:
             self.inflated_size = data.getbuffer().nbytes
             fd.write(self.inflated_size.to_bytes(4, byteorder="little"))
             self.zlibdata = zlib.compress(data.getbuffer())
             fd.write(self.zlibdata)
-        
-    def Dump(self, filename):
+
+    def Dump(self, filename: str) -> None:
         with codecs.open(filename, "w", "utf-8") as fd:
             for entry in self.entries:
                 fd.write(f"{entry.key.string}={entry.value.string}\n")
 
-    #private
-    def __ParseFile(self):
-        self.data = io.BytesIO(zlib.decompress(self.zlibdata))
-        self.entry_count = self.__ReadUint32BE(self.data)
+    # Please Note that this function does not perform well reading a whole dump
+    # There are a lot of issues with encoding that makes it not work properly
+    # on special characters
+    def FromTextFile(self, filename: str) -> None:
+        with codecs.open(filename, "r", "utf-8") as fd:
+            data = fd.read()
+            regex = re.compile(r"((?:.*_).*)=(.*(?:[^=]*\n)*)")
+            matches = regex.findall(data)
+            for match in matches:
+                print(match)
+                self[match[0]] = match[1]
+
+    # private
+    def __ParseFile(self) -> None:
+        self.data: io.BytesIO = io.BytesIO(zlib.decompress(self.zlibdata))
+        self.entry_count: int = self.__ReadUint32BE(self.data)
 
         while len(self.entries) < self.entry_count:
             self.entries.append(Entry.FromBytesIO(self.data))
-        pass
 
-    def __ReadUint32BE(self, data):
+    def __ReadUint32BE(self, data: io.BytesIO) -> int:
         byte = data.read(4)
         return int.from_bytes(byte, byteorder="big")  # whar?
-    
-    def __WriteUint32BE(self, number):
+
+    def __WriteUint32BE(self, number: int) -> bytes:
         return number.to_bytes(4, byteorder="big")
-    
-    #overrides
-    def __setitem__(self, key, value):
+
+    # overrides
+    def __setitem__(self, key: str, value: str) -> None:
         for entry in self.entries:
             if entry.key.string == key:
                 entry.SetValue(value)
                 return
         self.entries.append(Entry.FromKeyValuePair(key, value))
         self.entry_count += 1
-        
-    def __getitem__(self, key):
+
+    def __getitem__(self, key: str) -> (str | None):
         for entry in self.entries:
             if entry.key.string == key:
                 return entry.value.string
--- README.md
+++ README.md
@@ -8,16 +8,30 @@ To use it just construct a new **LangFile** object. You can treat the object lik
 
 ### Example
 
+#### Example.py:
+
 ```py
 from DecodeLang import LangFile
 
 if __name__ == "__main__":
     langfile = LangFile("language.1.bin") # open file
+    langfile.FromTextFile("Examples.txt") # read a list of new entries. This is NOT reliable
     langfile["test"] = "test" # create new entry
     langfile["UI_PHASE_DESCRIPTION_SUCCESS_NONE"] = "Launched your mom into space" # change existing entry
     print(langfile['MirageColorSchemeType_Black_DisplayName']) # read entry
     langfile.Save("language.1.bin.edit") # save file
     langfile.Dump("language.1.dump")
+```
+
+#### Examples.txt:
+
+```properties
+ColorSchemeType_Ranked2_DisplayName=Goldforged
+MonikerType_Account17=Legend XII
+EmojiType_VDayNaiHeart_DisplayName=Date Night Heart
+UI_Account_Linking_Header=Account Linking
+SevenColorSchemeType_Summer_DisplayName=Seven Heatwave
+CostumeType_MadKing_DisplayName=The Mad King
 ```
 
 ## Documentation
